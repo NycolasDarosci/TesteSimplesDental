ad14e78b33318394a9cf5d9de04f2cabd36b5215
Nycolas Darosci <Robert8765@protonmail.com>
2024-10-09 13:31:27 -0300

    commit inicial: estrutura projeto spring boot

---
+++ src/test/java/com/gerenciador/profissionais/ProfissionaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciador.profissionais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfissionaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
